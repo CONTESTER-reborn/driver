e11df53011f96b62f8ba7ccd8e560bcf6a4a75f8
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-12-25 20:03:50 +0300

    feat(config): new variables

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ from pathlib import Path
 
 ROOT_DIR = Path(__file__).resolve().parent
 LOCAL_USER_SCRIPTS_DIR = ROOT_DIR / 'tmp'
-DOCKER_USER_SCRIPTS_DIR = 'user-scripts'
-DOCKER_COMPILED_FILES_DIR = 'compiled-scripts'
+DOCKER_USER_SCRIPTS_DIR = 'user-scripts-dir'
+DOCKER_COMPILED_FILES_DIR = 'compiled-scripts-dir'
+DOCKER_TIME_OUTPUT_FILE = 'time-output-file'
