ec17e360794cb05386e077157040a91ad485cd39
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-12-26 00:28:37 +0300

    chore(config): changed value of `DOCKER_TIME_OUTPUT_FILE`

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ ROOT_DIR = Path(__file__).resolve().parent
 LOCAL_USER_SCRIPTS_DIR = ROOT_DIR / 'tmp'
 DOCKER_USER_SCRIPTS_DIR = 'user-scripts-dir'
 DOCKER_COMPILED_FILES_DIR = 'compiled-scripts-dir'
-DOCKER_TIME_OUTPUT_FILE = 'time-output-file'
+DOCKER_TIME_OUTPUT_FILE = 'time-stdout-file'
