f61e5f602de59c4a29794045a94d4a81f461d06d
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-12-25 20:05:19 +0300

    refactor(files): filename generation moved to separate method, initialization in __init__

--- driver/libs/files/__init__.py
+++ driver/libs/files/__init__.py
@@ -10,18 +10,24 @@ ProgrammingLanguagesMember = t.TypeVar('ProgrammingLanguagesMember', bound=Progr
 
 
 class FileCreator:
-    def __enter__(self, source_code: str, programming_language: ProgrammingLanguagesMember):
-        # Generating unique name of the file
-        name = str(uuid.uuid4())
-        extension = programming_language
-        self.filename = f'{name}.{extension}'
+    def __init__(self, source_code: str, programming_language: ProgrammingLanguages):
+        self.__source_code = source_code
+        self.__programming_language = programming_language
 
+    def __generate_filename(self) -> Filename:
+        name = str(uuid.uuid4())
+        extension = self.__programming_language.value.file_extension
+        return f'{name}.{extension}'
+
+    def __enter__(self):
+        # Generating unique name of the file
+        self.filename = self.__generate_filename()
         # Building path to file
         self.__file_path = LOCAL_USER_SCRIPTS_DIR / self.filename
 
         # Writing passed source code to the file
         with open(self.__file_path, 'w') as file:
-            file.write(source_code)
+            file.write(self.__source_code)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
