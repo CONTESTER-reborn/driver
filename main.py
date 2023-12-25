12416cf1fca611207a008b5892a0e12b3ffb3c04
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-12-26 00:46:50 +0300

    chore(main): lines are rearranged

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ Container = ContainersFactory.get(language)
 source = """print(1)\nprint(2)\nprint(3)"""
 
 with Container(time_limit=2, memory_limit='128m') as container:
-    for _ in range(10):
-        with FileCreator(source_code=source, programming_language=language) as file_creator:
+    with FileCreator(source_code=source, programming_language=language) as file_creator:
+        for _ in range(10):
             execution_options = CodeExecutionCommandOptions(filename=file_creator.filename, stdin=r'')
             result = container.execute(options=execution_options)
             print(result)
