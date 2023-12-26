cdedc3f99103449ed3637223446185586da40cee
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-12-27 01:18:17 +0300

    tests(libs): added tests of container lifecycle

---
+++ tests/test_libs/test_containers_lifecycle.py
@@ -0,0 +1,22 @@
+from time import sleep
+
+import pytest
+from docker import from_env
+
+from driver.libs.containers import ContainersFactory
+from driver.libs.enums import ProgrammingLanguage
+
+
+@pytest.fixture
+def client():
+    client = from_env()
+    yield client
+
+
+def test_container_context_menu(client):
+    ContainerClass = ContainersFactory.get(language=ProgrammingLanguage.PYTHON)
+    with ContainerClass(time_limit=1, memory_limit='128m') as container:
+        actual_container = container._container
+        assert actual_container in client.containers.list()
+
+    assert actual_container not in client.containers.list()
